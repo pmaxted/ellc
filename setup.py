1ab43b6d68a1c0c0982f5db4f18aa2726f177a85
zzx <zzx@bigtree.xyz>
2024-11-13 20:29:19 +0800

    update setup.py, using makefile to compile fortran code

--- setup.py
+++ setup.py
@@ -1,44 +1,34 @@
 #!/usr/bin/env python
 import setuptools
-from os.path import join
+import subprocess
+# from os.path import join
+
+
+def run_make():
+  subprocess.check_call(['make'])
+  subprocess.check_call(['make', 'install'])
+
 
 def configuration(parent_package='', top_path=None):
-  from numpy.distutils.misc_util import Configuration
+  run_make()
 
-  config = Configuration('ellc', parent_package, top_path)
+  package_data = {
+    'ellc': ['data/*', 'doc/*', 'examples/*']
+  }
 
-  # Order of source files in order of dependencies.
-  sources = [ 'ellc_f.pyf',
-     join('f_src','constants.f90'),
-     join('f_src','utils.f90'),
-     join('f_src','coords.f90'),
-     join('f_src','gauss_legendre.f90'),
-     join('f_src','solve_real_poly.f90'),
-     join('f_src','ellipse.f90'),
-     join('f_src','stellar.f90'),
-     join('f_src','spots.f90'),   
-     join('f_src','ellc.f90')
-  ]
+  return [], package_data
 
-  config.add_extension('ellc_f', sources=sources)
-
-  config.add_data_dir('data')
-
-  config.add_data_dir('doc')
-
-  config.add_data_dir('examples')
-
-  return config
 
 if __name__ == '__main__':
-    from numpy.distutils.core import setup
-
     try:
       from version import __version__
     except:
       __version__ = ''
 
-    setup(
+    ext_modules, package_data = configuration()
+
+    setuptools.setup(
+        name='ellc',
         version=__version__,
         author='Pierre Maxted',
         author_email='p.maxted@keele.ac.uk',
@@ -52,6 +42,9 @@ if __name__ == '__main__':
           'Topic :: Scientific/Engineering',
           'Programming Language :: Python',
           'Programming Language :: Fortran'],
-          install_requires=["numpy >= 1.10.0","astropy >= 1.1.1"], 
-        **configuration(top_path='').todict())
-
+        install_requires=["numpy >= 1.10.0","astropy >= 1.1.1"],
+        ext_modules=ext_modules,
+        packages=['ellc'],
+        package_data=package_data,
+        include_package_data=True
+    )
